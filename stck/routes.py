05800be64ef4f75fe199f4c4685eab471d01efd4
AlexStraubhaar <straubhaaralex@gmail.com>
2019-05-18 16:00:09 +0200

    Basic user login wip

--- stck/routes.py
+++ stck/routes.py
@@ -1,17 +1,35 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from stck import app
 from stck.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from stck.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Alex'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login required for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
