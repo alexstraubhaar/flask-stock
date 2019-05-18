727da8ae172970bf7680aab2537fe10bfaa5dc80
AlexStraubhaar <straubhaaralex@gmail.com>
2019-05-18 15:07:20 +0200

    Basic routes and template

--- stck/forms.py
+++ stck/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
--- stck/routes.py
+++ stck/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from stck import app
 from stck.forms import LoginForm
 
@@ -8,7 +8,10 @@ def index():
     user = {'username': 'Alex'}
     return render_template('index.html', title='Home', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login required for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
