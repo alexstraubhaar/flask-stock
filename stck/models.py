876f2c012840c3c7344c40144884a25e15c93375
AlexStraubhaar <straubhaaralex@gmail.com>
2019-05-18 15:37:21 +0200

    Artists and Albums table

--- stck/models.py
+++ stck/models.py
@@ -12,4 +12,15 @@ class User(db.Model):
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    
+    albums = db.relationship('Album', backref='artist', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Artist {}>'.format(self.name)
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+
+    def __repr__(self):
+        return '<Album {}>'.format(self.title)
